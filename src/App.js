551acd448d4bb1f321621a6047d377f15661cb3b
Dhanushroyal123 <dhanushroyal516@gmail.com>
2023-12-01 09:01:16 +0530

    app modify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import homepic from './assets/home.jpg'
 import './App.css'
 import Header from './components/Header/header'
 
 function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <h1>Hello</h1>
+    </>
+  )
 }
 
 export default App
